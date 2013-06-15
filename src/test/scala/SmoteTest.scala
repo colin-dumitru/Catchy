2e770b7ea8d228fce3ae165eb8b28212ed0b8c97
Catalin Dumitru <colin.dumitru@gmail.com>
2013-06-15 18:04:26 +0300

    added smote test

---
+++ src/test/scala/SmoteTest.scala
@@ -0,0 +1,16 @@
+package scala
+
+import org.scalatest.{BeforeAndAfter, FunSuite}
+
+/**
+ * Catalin Dumitru
+ * Date: 6/15/13
+ * Time: 6:01 PM
+ */
+class SmoteTest extends FunSuite with BeforeAndAfter{
+
+  test("Smoke Test") {
+    assert(1 == 1)
+  }
+
+}

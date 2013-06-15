38a4b0059d453cf97cfc762bddd869d5201349be
Catalin Dumitru <colin.dumitru@gmail.com>
2013-06-15 18:30:37 +0300

    updated fxml file

--- src/edu.rf.ui/Main.java
+++ src/edu.rf.ui/Main.java
@@ -9,9 +9,9 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/layout/sample.fxml"));
-        primaryStage.setTitle("Hello World");
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/layout/main.fxml"));
+        primaryStage.setTitle("RichFeed");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
